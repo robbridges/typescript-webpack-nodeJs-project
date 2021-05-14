71dc7ba1552a7474941fb571cd5d53eec0dce6ce
robbridges <robbridges@users.noreply.github.com>
2021-05-13 17:20:10 -0700

    Setting up web pack

---
+++ dist/Server.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Server = void 0;
+var Server = /** @class */ (function () {
+    function Server() {
+    }
+    Server.prototype.createServer = function () {
+        console.log('created server');
+    };
+    return Server;
+}());
+exports.Server = Server;
